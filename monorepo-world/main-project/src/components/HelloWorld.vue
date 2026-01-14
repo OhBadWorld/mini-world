f412d476ca132158e2c016e544bb42738c777e11
xichaoqun <xichaoqun@able-elec.com>
2026-01-14 15:18:30 +0800

    feat: 添加 插件 vue-countup-v3

--- monorepo-world/main-project/src/components/HelloWorld.vue
+++ monorepo-world/main-project/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import CountUp from 'vue-countup-v3';
 import { hello, randomString } from 'common'
 
 defineProps<{ msg: string }>()
@@ -10,31 +11,22 @@ console.log('randomString:', randomString())
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
+    <count-up :endVal="1000" :duration="2" />
+  
+    <div class="card">
+      <button type="button" @click="count++">count is {{ count }}</button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+
 </template>
 
 <style scoped>
